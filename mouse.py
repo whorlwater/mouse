14bf443499ced64703406e401fa341b8db1b1f34
snailtsunami <snail@solitarysnail.com>
2012-07-19 01:56:06 -0700

    randomly choose known room instead of unknown

--- mouse.py
+++ mouse.py
@@ -2,64 +2,78 @@ from selenium import webdriver
 from time import sleep
 from random import shuffle
 from re import sub
+from random import randrange
 
 def loadMaze():
 	browser = webdriver.Firefox()
-	browser.get('file:///home/snail/Documents/code/mouse/rooms/room39.html')
+	browser.get('file:///home/snail/Documents/code/mouse/rooms/room957.html')
 	return browser
 
 def chooseDoor(maze,knownRooms):
-	doorList = []
-	idealDoorList = []
-	doorList = maze.find_elements_by_xpath('//a')
-	for door in doorList:
+	def identifyDoors(door):
 		doorURL = door.get_attribute('href')
 		doorNumber = sub('^file.*rooms\/room|.html','',doorURL)
 		if doorNumber not in knownRooms:
-			knownRooms.append(doorNumber)
-			idealDoorList.append(door)
-	if len(idealDoorList) >= 1:
-		print 'Chose ideal door.'
-		print idealDoorList[0].get_attribute('href')
-		return idealDoorList[0]
+			unknownDoorList.append(door)
+		else:
+			knownDoorList.append(door)
+	def chooseUnknown(unknownDoorList):
+		print 'Chose unknown door.' #Debugging
+		return unknownDoorList[0]
+	def chooseKnown(knownDoorList):
+		print 'Chose known door.' #Debugging
+		shuffle(knownDoorList)
+		return knownDoorList[0]
+	def chooseMixed(knownDoorList,unknownDoorList):
+		if randrange(1,4) != 2:
+			return chooseUnknown(unknownDoorList)
+		else:
+			return chooseKnown(knownDoorList)
+	doorList = []
+	knownDoorList = []
+	unknownDoorList = []
+	
+	doorList = maze.find_elements_by_xpath('//a')
+
+	[identifyDoors(door) for door in doorList]
+
+	if len(unknownDoorList) and len(knownDoorList) != 0:
+		print 'Mixed doors.' #Debugging
+		return chooseMixed(knownDoorList,unknownDoorList)
 	else:
-		print 'Chose not ideal door.'
-		return doorList[0]
+		if len(unknownDoorList) == 0:
+			print 'Only known doors.' #Debugging
+			return chooseKnown(knownDoorList)
+		else:
+			print 'Only unknown doors.' #Debugging
+			return chooseUnknown(unknownDoorList)
 
 def runMaze(maze):
-
 	knownRooms = []
-
 	exitFound = 0
-
 	moveCounter = 0 #Debugging
 
 	while exitFound == 0:
 		roomNumber = maze.find_elements_by_id('roomNumber')
 		while len(roomNumber) < 1:
 			roomNumber = maze.find_elements_by_id('roomNumber')
-		roomNumber = roomNumber[0].text
-		roomNumber = roomNumber.replace('Room ','')
+		roomNumber = roomNumber[0].text.replace('Room ','')
 		if roomNumber not in knownRooms:
 			knownRooms.append(roomNumber)
 
 		messages = maze.find_elements_by_id('message')
 		
-		if len(messages) > 0:
-			message = messages[0].text
-		else:
-			message = ''
-
-		if message == 'YOU WIN!':
+		if 'YOU WIN!' in messages:
 			exitFound = 1
 			print message
 			print "You escape the maze!"
 		else:
+			moveCounter = moveCounter + 1 #Debugging
+			print moveCounter #Debugging
 			door = chooseDoor(maze,knownRooms)
-			moveCounter = moveCounter + 1
-			print moveCounter
 			door.click()
-
+	
+	print knownRooms.sort() #Debugging
 	maze.close()
 
 maze = loadMaze()
